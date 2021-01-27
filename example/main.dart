826db86375f38d976964b1a3b9cfe979e02a866f
Prikesh Savla <prikeshsavla@gmail.com>
2021-01-27 22:06:44 +0530

    Add support for generate method with email and date as inputs with name

--- example/main.dart
+++ example/main.dart
@@ -4,17 +4,24 @@ import 'package:username_generator/username_generator.dart';
 void main() {
   var generator = UsernameGenerator();
 
+  //default separator is "_"
+  generator.separator = "_"; // optional
+
   print(generator.generateRandom());
   // limitation_virulent_1
 
-  print(generator.generateForName("John"));
-  // john_47
+  print(generator.generate("John"));
+  // john_75
 
-  print(generator.generateForName("John", lastName: "Doe"));
-  // 36_john_doe
+  print(generator.generate("John Doe"));
+  // john_doe_6
 
-  print(generator.generateForName("John", lastName: "Doe", hasNumbers: false));
-  // doe_john
+  print(generator.generate("John Doe", hasNumbers: false));
+  // john_doe
+
+  print(generator.generate("johndoe@example.com",
+      date: DateTime.parse("1996-01-15")));
+  // johndoe_96
 
   print(
     generator.generateForName("John",
@@ -23,8 +30,8 @@ void main() {
   // doe_john_10_ready
 
   print(
-    generator.generateListForName("John",
-        lastName: "Doe", adjectives: ["ready", "happy"], length: 5),
+    generator.generateList("John Doe",
+        adjectives: ["ready", "happy"], length: 5),
   );
   // [
   // happy_doe_john_12,
--- test/username_generator_test.dart
+++ test/username_generator_test.dart
@@ -4,8 +4,8 @@ import 'package:username_generator/username_generator.dart';
 
 void main() {
   group('generate a random username', () {
-    final usernameGenerator = UsernameGenerator();
-    var username = usernameGenerator.generateRandom();
+    final generator = UsernameGenerator();
+    var username = generator.generateRandom();
     test('where username contains numbers', () {
       var maxThreeDigits = RegExp(
         r"\d{1,3}",
@@ -26,8 +26,8 @@ void main() {
     });
   });
   group('generate a username for first name John', () {
-    final usernameGenerator = UsernameGenerator();
-    var username = usernameGenerator.generateForName("John");
+    final generator = UsernameGenerator();
+    var username = generator.generateForName("John");
     var maxThreeDigits = RegExp(
       r"\d{1,3}",
       caseSensitive: false,
@@ -50,19 +50,75 @@ void main() {
     });
     test('where hasNumbers is false ', () {
       expect(
-          maxThreeDigits.hasMatch(
-              usernameGenerator.generateForName("John", hasNumbers: false)),
+          maxThreeDigits
+              .hasMatch(generator.generateForName("John", hasNumbers: false)),
           false);
     });
   });
 
   group('generate a list of usernames for John Doe', () {
-    final usernameGenerator = UsernameGenerator();
+    final generator = UsernameGenerator();
 
     test('where it returns a list of 5 items', () {
-      var usernames = usernameGenerator.generateListForName("John",
-          lastName: "Doe", length: 5);
+      var usernames =
+          generator.generateListForName("John", lastName: "Doe", length: 5);
       expect(usernames, allOf([hasLength(5), isA<List<String>>()]));
+    });
+  });
+
+  group('generate a username for a name "John Doe"', () {
+    final generator = UsernameGenerator();
+    var username = generator.generate("John Doe");
+    var maxThreeDigits = RegExp(
+      r"\d{1,3}",
+      caseSensitive: false,
+      multiLine: false,
+    );
+    test('where username contains numbers', () {
+      expect(maxThreeDigits.hasMatch(username), true);
+    });
+
+    test('where username contains letters', () {
+      var containsLetters = RegExp(
+        r"[a-z]+",
+        caseSensitive: false,
+        multiLine: false,
+      );
+      expect(containsLetters.hasMatch(username), true);
+    });
+    test('where username contains the word john', () {
+      expect(username, contains("john"));
+    });
+
+    test('where username contains the word doe', () {
+      expect(username, contains("doe"));
+    });
+    test('where hasNumbers is false ', () {
+      expect(
+          maxThreeDigits
+              .hasMatch(generator.generateForName("John", hasNumbers: false)),
+          false);
+    });
+  });
+
+  group('generate for "john.doe55@example.com" and date "15-01-1996"', () {
+    final generator = UsernameGenerator();
+    var username = generator.generate("john.doe55@example.com",
+        date: DateTime.parse("1996-01-15"));
+
+    test('where username contains the word johndoe', () {
+      expect(username, contains("johndoe55"));
+    });
+
+    test('where username contains the word 15 or 01 or 1996 or 96', () {
+      expect(
+          username,
+          anyOf([
+            contains("15"),
+            contains("01"),
+            contains("1996"),
+            contains("96"),
+          ]));
     });
   });
 }
